426486f8b8c9e2c11621b1744ac7cb184592a611
C107156136 <C107156136@nkust.edu.tw>
2021-04-01 11:57:45 +0800

    0401 homework

---
+++ homework-0401/App3.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <Car/>;
+  }
+}
+
+
+
+
+class Car extends React.Component {
+  render() {
+    return <h2>class example</h2>;
+  }
+}
+
+export default App;
---
+++ homework-0401/App4.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App=()=>(
+	 <Car/>
+)
+
+const Car=()=>(
+	<h2>const arrow example</h2>
+)
+
+export default App;
